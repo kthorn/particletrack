dcdbc22459233765604a3ecc804e7a7a451b0390
Kurt <kurt.thorn@ucsf.edu>
2011-04-26 10:46:59 -0700

    Fixed MM parsing / reading code to handle wavelength names with underscores

--- MMstruct.m
+++ MMstruct.m
@@ -3,6 +3,10 @@ function MMstructure = MMstruct (directory)
 %MMstruct - returns the structure of the Micro-Manager data set in
 %directory
 
+%regular expression for parsing filenames
+RE = 'img_(\d+)_(.+)_(\d+).tif';
+prefix = 'img';
+
 files=dir(fullfile(directory,'*.tif'));
 max_time=0;
 max_z=0;
@@ -10,17 +14,17 @@ nwaves=1;
 wavelengths={};
 for n=1:max(size(files))
     if ~files(n).isdir
-        fileparts = regexp(files(n).name, '_', 'split');
-        prefix = fileparts{1};
-        max_time=max(max_time,str2double(fileparts{2}));
-        z = regexp(fileparts{4}, '\.', 'split');
+        fileparts = regexp(files(n).name, RE, 'tokens');
+        fileparts = fileparts{1};
+        max_time=max(max_time,str2double(fileparts{1}));
+        z = regexp(fileparts{3}, '\.', 'split');
         max_z=max(max_z,str2double(z{1}));
         if isempty(wavelengths)
-            wavelengths{nwaves}=fileparts{3};
+            wavelengths{nwaves}=fileparts{2};
         else
-            if ~any(strcmpi(wavelengths,fileparts{3}))
+            if ~any(strcmpi(wavelengths,fileparts{2}))
                 nwaves=nwaves+1;
-                wavelengths{nwaves}=fileparts{3};
+                wavelengths{nwaves}=fileparts{2};
             end
         end
     end
